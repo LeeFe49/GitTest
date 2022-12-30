a1c7e7142bc594ced0559629b42b3445e9bb8973
Cheol Lee <chullee49@naver.com>
2022-12-30 20:53:32 +0900

    Admin Constructor 추가, controller 수정

--- Test1/src/Controller/temp.java
+++ Test1/src/Controller/temp.java
@@ -1,7 +1,9 @@
 package Controller;
 
 import dto.Account;
+import dto.Admin;
 
 public class temp {
     Account account = new Account("jin", "1234", 28);
+    Admin admin = new Admin("cheol", "1234", 24);
 }
--- Test1/src/dto/Admin.java
+++ Test1/src/dto/Admin.java
@@ -5,9 +5,13 @@ public class Admin {
     private String password;
     private int age;
 
-    Admin(String name, String password, int age) {
+    public Admin(String name, String password, int age) {
         this.name = name;
         this.password = password;
         this.age = age;
+    }
+
+    public Admin() {
+
     }
 }
