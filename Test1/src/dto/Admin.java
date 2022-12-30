23e556134129d1a92356dfda1f8cbcb42494029e
Cheol Lee <chullee49@naver.com>
2022-12-30 20:24:44 +0900

    admin 추가

---
+++ Test1/src/dto/Admin.java
@@ -0,0 +1,13 @@
+package dto;
+
+public class Admin {
+    private String name;
+    private String password;
+    private int age;
+
+    Admin(String name, String password, int age) {
+        this.name = name;
+        this.password = password;
+        this.age = age;
+    }
+}
