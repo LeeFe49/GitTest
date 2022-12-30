d19cfcd4eb0874f1cbaa1131d0186cfd980c688d
Cheol Lee <chullee49@naver.com>
2022-12-30 20:36:58 +0900

    Main 수정 Controller 패키지 추가 Account 클래스 추가

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Second Hi!");
-        System.out.println("change LocalRepository");
-        System.out.println("메인 계속해서 작업 제개");
-    }
-
-    public void Hi() {
-        System.out.println("new Func");
     }
 }
---
+++ Test1/src/dto/Account.java
@@ -0,0 +1,14 @@
+package dto;
+
+public class Account {
+    private String name;
+    private String password;
+    private int age;
+
+    Account(String name, String password, int age) {
+        this.name = name;
+        this.password = password;
+        this.age = age;
+    }
+
+}
